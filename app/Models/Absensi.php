f12bc8c51db8c0688f2c4b9a52204ede63dd51e7
azrilFitrayana <azrilfitrayana77@gmail.com>
2024-06-10 18:57:13 +0700

    membuat model, migration, controller

---
+++ app/Models/Absensi.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Absensi extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id_karyawan',
+        'status_absen',
+        'keterangan',
+        'tanggal_absensi',
+    ];
+
+    public function karyawan(){
+        return $this->hasMany(Karyawan::class);
+    }
+}
